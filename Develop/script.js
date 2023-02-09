f4a686dce88fb8121285b1ca61813dcc70d6af73
Elvis Diaz <elvisdiaz020@gmail.com>
2023-02-09 01:03:41 -0500

    adds uppercase letter functionality for password generator

--- Develop/script.js
+++ Develop/script.js
@@ -10,11 +10,49 @@ function writePassword() {
 }
 
 function generatePassword() {
-  confirm("Do you want to have uppercase letters?");
+  addUpperCaseLetters();
   confirm("Do you want to have lowercase letters?");
   confirm("Do you want to have special characters?");
+  confirm("How many letters do you want?");
   console.log("apple");
-  return "apple";
+  return addUpperCaseLetters();
+}
+
+function addUpperCaseLetters() {
+  let upperCaseAlpha = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  confirm("Do you want to have uppercase letters?");
+  if (confirm) {
+    let randomIndex = Math.floor(Math.random() * upperCaseAlpha.length);
+    let randomAlpha = upperCaseAlpha[randomIndex];
+    console.log(randomAlpha);
+    return randomAlpha;
+  }
 }
 
 // Add event listener to generate button
