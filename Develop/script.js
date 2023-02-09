254cb219cb85e0d5eba3353ee702474c347679e4
Elvis Diaz <elvisdiaz020@gmail.com>
2023-02-09 00:50:18 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePassword() {
+  confirm("Do you want to have uppercase letters?");
+  confirm("Do you want to have lowercase letters?");
+  console.log("apple");
+  return "apple";
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
