3c27732b99abf5992b446bb6e75f9173de7569d9
Elvis Diaz <elvisdiaz020@gmail.com>
2023-02-09 19:15:48 -0500

    adds stable password uppercase generator

--- Develop/script.js
+++ Develop/script.js
@@ -57,6 +57,7 @@ let lowerCaseAlpha = [
 
 let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "/", ";", ":"];
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let newArray = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -76,81 +77,87 @@ function passwordSize() {
   } else if (ask > 128) {
     alert("Please make a password of less than 128 characters");
   }
-}
+  function passwordCreation() {
+    let askUpper = confirm("Do you want to have uppercase letters?");
+    let askLower = confirm("Do you want to have lowercase letters?");
+    let askSpecial = confirm("Do you want to have special characters?");
+    let askNum = confirm("Do you want to have numbers?");
 
-function passwordCreation() {
-  let askUpper = confirm("Do you want to have uppercase letters?");
-  let askLower = confirm("Do you want to have lowercase letters?");
-  let askSpecial = confirm("Do you want to have special characters?");
-  let askNum = confirm("Do you want to have numbers?");
-
-  if (askUpper) {
-    let randomIndex = Math.floor(Math.random() * upperCaseAlpha.length);
-    let randomAlpha = upperCaseAlpha[randomIndex];
-    return randomAlpha;
-  } else if (askLower) {
-    let randomIndex = Math.floor(Math.random() * lowerCaseAlpha.length);
-    let randomAlpha = lowerCaseAlpha[randomIndex];
-    return randomAlpha;
-  } else if (askSpecial) {
-    let randomIndex = Math.floor(Math.random() * specialChar.length);
-    let randomChar = specialChar[randomIndex];
-    return randomChar;
-  } else if (askNum) {
-    let randomIndex = Math.floor(Math.random() * numbers.length);
-    let randomNum = numbers[randomIndex];
-    return randomNum;
+    if (askUpper) {
+      for (let index = 0; index < ask; index++) {
+        let randomIndex = Math.floor(Math.random() * upperCaseAlpha.length);
+        let randomAlpha = upperCaseAlpha[randomIndex];
+        newArray.push(randomAlpha);
+      }
+      let result = newArray.join("");
+      console.log(result);
+      return result;
+    } else if (askLower) {
+      let randomIndex = Math.floor(Math.random() * lowerCaseAlpha.length);
+      let randomAlpha = lowerCaseAlpha[randomIndex];
+      return randomAlpha;
+    } else if (askSpecial) {
+      let randomIndex = Math.floor(Math.random() * specialChar.length);
+      let randomChar = specialChar[randomIndex];
+      return randomChar;
+    } else if (askNum) {
+      let randomIndex = Math.floor(Math.random() * numbers.length);
+      let randomNum = numbers[randomIndex];
+      return randomNum;
+    }
   }
+
+  return passwordCreation();
 }
 
 function generatePassword() {
-  passwordSize();
-  passwordCreation;
+  // passwordSize();
+  // passwordCreation;
   // addUpperCaseLetters;
   // addLowerCaseLetters;
   // addSpecialChar;
   // addNumbers;
   // confirm("How many letters do you want?");
   console.log("apple");
-  return passwordCreation();
+  return passwordSize();
 }
 
-function addUpperCaseLetters() {
-  let ask = confirm("Do you want to have uppercase letters?");
-  if (ask) {
-    let randomIndex = Math.floor(Math.random() * upperCaseAlpha.length);
-    let randomAlpha = upperCaseAlpha[randomIndex];
-    console.log(randomAlpha);
-    return randomAlpha;
-  }
-}
-function addLowerCaseLetters() {
-  let ask = confirm("Do you want to have lowercase letters?");
-  if (ask) {
-    let randomIndex = Math.floor(Math.random() * lowerCaseAlpha.length);
-    let randomAlpha = lowerCaseAlpha[randomIndex];
-    console.log(randomAlpha);
-    return randomAlpha;
-  }
-}
-function addSpecialChar() {
-  let ask = confirm("Do you want to have special characters?");
-  if (ask) {
-    let randomIndex = Math.floor(Math.random() * specialChar.length);
-    let randomChar = specialChar[randomIndex];
-    console.log(randomChar);
-    return randomChar;
-  }
-}
-function addNumbers() {
-  let ask = confirm("Do you want to have numbers?");
-  if (ask) {
-    let randomIndex = Math.floor(Math.random() * numbers.length);
-    let randomNum = numbers[randomIndex];
-    console.log(randomNum);
-    return randomNum;
-  }
-}
+// function addUpperCaseLetters() {
+//   let ask = confirm("Do you want to have uppercase letters?");
+//   if (ask) {
+//     let randomIndex = Math.floor(Math.random() * upperCaseAlpha.length);
+//     let randomAlpha = upperCaseAlpha[randomIndex];
+//     console.log(randomAlpha);
+//     return randomAlpha;
+//   }
+// }
+// function addLowerCaseLetters() {
+//   let ask = confirm("Do you want to have lowercase letters?");
+//   if (ask) {
+//     let randomIndex = Math.floor(Math.random() * lowerCaseAlpha.length);
+//     let randomAlpha = lowerCaseAlpha[randomIndex];
+//     console.log(randomAlpha);
+//     return randomAlpha;
+//   }
+// }
+// function addSpecialChar() {
+//   let ask = confirm("Do you want to have special characters?");
+//   if (ask) {
+//     let randomIndex = Math.floor(Math.random() * specialChar.length);
+//     let randomChar = specialChar[randomIndex];
+//     console.log(randomChar);
+//     return randomChar;
+//   }
+// }
+// function addNumbers() {
+//   let ask = confirm("Do you want to have numbers?");
+//   if (ask) {
+//     let randomIndex = Math.floor(Math.random() * numbers.length);
+//     let randomNum = numbers[randomIndex];
+//     console.log(randomNum);
+//     return randomNum;
+//   }
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
